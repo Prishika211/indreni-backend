1f3db12acd654401bc5641c04dfe92a6b4e72ec4
prishika211 <prishikacdy10@gmail.com>
2024-10-26 18:33:02 +0530

    feat: add story model

---
+++ src/models/story.models.js
@@ -0,0 +1,35 @@
+import mongoose, {Schema} from "mongoose"
+
+const storySchema = new Schema (
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        decription: {
+            type: String,
+            required: true,
+        },
+        imageUrls: [
+            {
+                type: String,
+                required: true
+            }
+        ],
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'Uesr',
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['draft', 'published', 'archived'],
+            default: 'draft'
+        },
+        tags: [{type: String}] // Optional tags to categorize stories
+    },
+    {timestamps: true}
+)
+
+export const Story = mongoose.model("Story", storySchema)
