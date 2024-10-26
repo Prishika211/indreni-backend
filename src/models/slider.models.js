0033121b307f1187b8a7062ba8bdae1726ec54ec
prishika211 <prishikacdy10@gmail.com>
2024-10-26 18:32:53 +0530

    feat: add slider model

---
+++ src/models/slider.models.js
@@ -0,0 +1,29 @@
+import mongoose, {Schema} from "mongoose"
+
+const sliderSchema = new Schema(
+    {
+        imageUrl: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['active', 'inactive'],
+            default: 'active'
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        displayOrder: {
+            type: Number,
+            default: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Slider = mongoose.model("Slider", sliderSchema)
