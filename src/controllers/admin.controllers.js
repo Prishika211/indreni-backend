795ed31212e328168050562b771a3c2a39b41ddd
prishika211 <prishikacdy10@gmail.com>
2024-10-28 10:51:08 +0530

    feat: add refreshAccessToken endpoint

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -2,8 +2,9 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {Admin} from "../models/admin.models.js"
+import {jwt} from "jsonwebtoken"
 
-const generateAcessandRefreshToken = async (adminId) => {
+const generateAccessandRefreshToken = async (adminId) => {
     try {
         const admin = await Admin.findById(adminId);
         const accessToken = admin.generateAccessToken();
@@ -20,7 +21,8 @@ const generateAcessandRefreshToken = async (adminId) => {
         )
     }
 }
-const registerUser = asyncHandler (async (req, res) => {
+
+const registerAdmin = asyncHandler (async (req, res) => {
     const {email, fullName, username, password} = req.body;
 
     if(
@@ -84,93 +86,153 @@ const registerUser = asyncHandler (async (req, res) => {
 })
 
 // Admin Login to generate tokens
-// const loginAdmin = asyncHandler(async (req, res) => {
-//     const {email, password} = req.body;
+const loginAdmin = asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
 
-//     if(!email){
-//         throw new ApiError(
-//             400,
-//             "email is required"
-//         )
-//     }
-//     try {
-//         const admin = await Admin.findOne({email});
+    if(!email){
+        throw new ApiError(
+            400,
+            "email is required"
+        )
+    }
+    try {
+        const admin = await Admin.findOne({email});
 
-//         if (!admin) {
-//             throw new ApiError(
-//                 400,
-//                 "Invalid email or password"
-//             )
-//         }
+        if (!admin) {
+            throw new ApiError(
+                404,
+                "Invalid email or password"
+            )
+        }
 
-//         const isPasswordValid = await admin.isPasswordCorrect(password);
+        const isPasswordValid = await admin.isPasswordCorrect(password);
 
-//         if (!isPasswordValid) {
-//             throw new ApiError (
-//                 401,
-//                 "Invalid user credentials"
-//             )
-//         }
+        if (!isPasswordValid) {
+            throw new ApiError (
+                401,
+                "Invalid user credentials"
+            )
+        }
 
-//         const {accessToken, refreshToken} = await generateAcessandRefreshToken (
-//             admin._id
-//         )
+        const {accessToken, refreshToken} = await generateAccessandRefreshToken (
+            admin._id
+        )
 
-//         const loggedInAdmin = await Admin.findById(admin._id).select(
-//             "-password -refreshToken"
-//         )  
+        const loggedInAdmin = await Admin.findById(admin._id).select(
+            "-password -refreshToken"
+        )  
 
-//         const options = {
-//             httpOnly: true,
-//             secure: true
-//         }
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
 
-//         return res
-//         .status(200)
-//         .cookie("accessToken", accessToken, options)
-//         .cookie("refreshToken", refreshToken, options)
-//         .json(
-//             new ApiResponse(
-//                 200,
-//                 {
-//                     admin: loggedInAdmin,
-//                     accessToken,
-//                     refreshToken
-//                 },
-//                 "Admin logged in successfully"
-//             )
-//         )
-//     } catch (error) {
-//         throw new ApiError(
-//             500,
-//             "Admin can't be logged in"
-//         )
-//     }
-// })
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    admin: loggedInAdmin,
+                    accessToken,
+                    refreshToken
+                },
+                "Admin logged in successfully"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Admin can't be logged in"
+        )
+    }
+})
 
-// const logoutAdmin = asyncHandler(async (req, res) => {
-//     await Admin.findByIdAndUpdate(
-//         req.admin._id, 
-//         {
-//             $unset: {
-//                 refreshToken: 1
-//             }
-//         },
-//         {
-//             new: true,
-//         }
-//     )
+const logoutAdmin = asyncHandler(async (req, res) => {
+    try {
+            await Admin.findByIdAndUpdate(
+                req.admin._id, 
+                {
+                    $unset: {
+                        refreshToken: 1
+                    }
+                },
+                {
+                    new: true,
+                }
+            )
+        
+            const options = {
+                httpOnly: true,
+                secure: true
+            }
+        
+            return res
+                .status(200)
+                .clearCookie("accessToken", options)
+                .clearCookie("refreshToken", options)
+                .json(new ApiResponse(200, {}, "Admin logged out successfully"))
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Admin can't be logged out"
+        )
+    }
+})
 
-//     const options = {
-//         httpOnly: true,
-//         secure: true
-//     }
+const refreshAccessToken = asyncHandler(async (req, res) => {
+   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
 
-//     return res
-//         .status(200)
-//         .clearCookie("accessToken", options)
-//         .clearCookie("refreshToken", options)
-// })
+   if(incomingRefreshToken){
+    throw new ApiError(
+        401,
+        "unauthorized request"
+    )
+   }
 
 
-export {registerUser}
+   try {
+    const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+ 
+    const admin = await Admin.findById(decodedToken?._id)
+ 
+    if(!admin){
+     throw new ApiError(
+         401,
+         "Invalid refresh token"
+     )
+    }
+ 
+    if(incomingRefreshToken !== admin?.refreshToken){
+         throw new ApiError(
+             401,
+             "Refresh token is expired or used"
+         )
+     }
+ 
+     const options = {
+         httpOnly: true,
+         secure: true
+     }
+ 
+     const {accessToken, newRefreshToken} = await generateAccessandRefreshToken(admin._id)
+ 
+     return res
+     .status(200)
+     .cookie("accessToken", accessToken, options)
+     .cookie("refreshToken", newRefreshToken, options)
+     .json(
+         new ApiResponse(
+             200,
+             {accessToken, refreshToken: newRefreshToken},
+             "Access token refreshed successfully"
+         )
+     )
+   } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token")
+   }
+})
+
+export {registerAdmin , loginAdmin, logoutAdmin, refreshAccessToken}
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,7 +1,14 @@
 import {Router} from "express";
-import { registerUser } from "../controllers/admin.controllers.js";
+import { loginAdmin, registerAdmin, logoutAdmin, refreshAccessToken } from "../controllers/admin.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(registerAdmin)
+router.route("/login").post(loginAdmin)
+
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutAdmin)
+router.route("/refresh-token").post(refreshAccessToken)
 export default router
