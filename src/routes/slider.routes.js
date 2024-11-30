831d22516d9e48dc4be66f5ce827183abc0f6f4d
prishika211 <prishikacdy10@gmail.com>
2024-11-30 10:17:58 +0545

    feat: create slider route

---
+++ src/routes/slider.routes.js
@@ -0,0 +1,25 @@
+import {Router} from "express";
+import {
+    addSliderImage,
+    removeSliderImage,
+    getAllSliderImages,
+    updateDisplayOrder
+}from "../controllers/slider.controllers.js";
+import {upload} from "../middlewares/multer.middlewares.js";
+import {verifyJWT} from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/")
+    .get(getAllSliderImages)
+    .post(upload.single("image"), addSliderImage);
+
+router.route("/:sliderId")
+    .delete(removeSliderImage);
+
+router.route("/:sliderId/display-order")
+    .patch(updateDisplayOrder);
+
+export default router;
