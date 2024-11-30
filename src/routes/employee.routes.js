c5fe6037d972211d20889074f80e28af33e122a2
prishika211 <prishikacdy10@gmail.com>
2024-11-30 12:41:19 +0545

    feat: create employee route

---
+++ src/routes/employee.routes.js
@@ -0,0 +1,25 @@
+import {Router} from "express";
+import {
+    getEmployees,
+    createEmployee,
+    updateEmployee,
+    deleteEmployee}
+from "../controllers/employee.controllers.js"
+import {upload} from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router()
+
+//Ensure all routes are protected by admin authentication
+router.use(verifyJWT);
+
+router.route("/")
+    .get(getEmployees)
+    .post(upload.single("photo"), createEmployee); 
+
+router.route("/:employeeId")
+    .patch(upload.single("photo"), updateEmployee) 
+    .delete(deleteEmployee);
+
+export default router;
+
