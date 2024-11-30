1e9ee99730f282fca0c1d200cf777fe93aac1010
prishika211 <prishikacdy10@gmail.com>
2024-11-30 15:13:59 +0545

    create gallery route

---
+++ src/routes/gallery.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+    addImagesToGallery,
+    removeGalleryImage,
+    getAllGalleryImages,
+} from "../controllers/gallery.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+
+// Protect all routes with admin authentication
+router.use(verifyJWT);
+
+router.route("/")
+    .get(getAllGalleryImages)
+    .post(upload.single("image"), addImagesToGallery); // Handle image upload
+
+router.route("/:galleryId")
+    .delete(removeGalleryImage);
+
+export default router;
