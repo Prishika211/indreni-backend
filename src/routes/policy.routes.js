f4c3a4359c344cfd96b496ae3ee9222316dfc628
prishika211 <prishikacdy10@gmail.com>
2024-12-28 14:13:29 +0530

    add policy route

---
+++ src/routes/policy.routes.js
@@ -0,0 +1,24 @@
+import {Router} from "express";
+import {
+    getAllPolicies,
+    createOrUpdatePolicy,
+    deletePolicy,
+    getPolicy
+} from "../controllers/policy.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/")
+    .get(getAllPolicies)
+    .post(upload.single("policy"), createOrUpdatePolicy);
+
+router.route("/:policyId")
+    .delete(deletePolicy)
+    .get(getPolicy)
+    
+
+export default router;
