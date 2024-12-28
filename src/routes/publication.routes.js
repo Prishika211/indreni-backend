36881ce565d7f698f3f4d1b2159a6c8c6a8292cc
prishika211 <prishikacdy10@gmail.com>
2024-12-28 14:13:38 +0530

    add publication route

---
+++ src/routes/publication.routes.js
@@ -0,0 +1,26 @@
+import {Router} from "express";
+import {
+    getAllPublications,
+    createPublication,
+    updatePublication,
+    deletePublication,
+    getPublication
+} from "../controllers/publication.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/")
+    .get(getAllPublications)
+    .post(upload.single("publication"),createPublication);
+
+router.route("/:publicationId")
+    .patch(upload.single("publication"),updatePublication)
+    .delete(deletePublication)
+    .get(getPublication)
+    
+
+export default router;
