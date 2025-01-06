aedd3abf6bd9e4a0931232333facdc24ca4c8a56
prishika211 <prishikacdy10@gmail.com>
2025-01-06 21:38:45 +0530

    create notice route

---
+++ src/routes/notice.routes.js
@@ -0,0 +1,24 @@
+import {Router} from 'express';
+import {
+  createNotice,
+  getAllNotices,
+  updateNotice,
+  deleteNotice,
+  getNotice,
+} from '../controllers/notice.controllers.js';
+import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+router.route("/")
+    .get(getAllNotices)
+    .post(upload.single("image"), createNotice); // Handle image upload
+
+router.route("/:noticeId")
+    .patch(upload.single("image"), updateNotice) // Handle image upload
+    .delete(deleteNotice)
+    .get(getNotice);
+
+export default router;
