b728c11ef0b522351844590951b8ed7d3c904e71
prishika211 <prishikacdy10@gmail.com>
2025-02-05 19:14:25 +0530

    update the logic for image upload

--- src/controllers/employee.controllers.js
+++ src/controllers/employee.controllers.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import {Employee} from "../models/employee.models.js";
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+// import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const getEmployees = asyncHandler (async (req, res) => {
     const employees = await Employee.find();
@@ -18,28 +18,33 @@ const getEmployees = asyncHandler (async (req, res) => {
 
 const createEmployee = asyncHandler(async (req, res) => {
     const {name, position, category} =req.body;
-    const photoLocalPath = req.file?.path;
+    // const photoLocalPath = req.file?.path;
 
     try {
-        if(!name || !position || !category || !photoLocalPath){
-            throw new ApiError(
-                400,
-                "All fields (name, position, category, and photoUrl) are required"
-            )
-        }
+        // if(!name || !position || !category || !photoLocalPath){
+        //     throw new ApiError(
+        //         400,
+        //         "All fields (name, position, category, and photoUrl) are required"
+        //     )
+        // }
+        if (!req.file) {
+            throw new ApiError(400, "No image uploaded");
+          }
+        
     
-        const photo = await uploadOnCloudinary(photoLocalPath)
+        // const photo = await uploadOnCloudinary(photoLocalPath)
+        const photo = req.file.path;
 
-        if(!photo?.url) {
-            throw new ApiError(
-                400, 
-                "Error while uploading photo"
-            )
-        }
+        // if(!photo?.url) {
+        //     throw new ApiError(
+        //         400, 
+        //         "Error while uploading photo"
+        //     )
+        // }
 
         const newEmployee = await Employee.create(
             {
-                name, position, category, photoUrl: photo.url
+                name, position, category, photoUrl: photo
             }
         );
     
